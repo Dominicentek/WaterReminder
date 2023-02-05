59e8cd20a50732f3886749bd56ad827aab305878
domin <dominicentekgaming@gmail.com>
2023-02-05 10:05:39 +0100

    Removed command prompt on startup

--- Installer/src/main/java/Main.java
+++ Installer/src/main/java/Main.java
@@ -12,12 +12,10 @@ public class Main {
             return;
         }
         JOptionPane.showMessageDialog(null, "Welcome to the Water Reminder installer.\nOnce you click \"OK\", the installation will begin.", "Water Reminder Installer", JOptionPane.INFORMATION_MESSAGE);
-        File startupFolder = new File(System.getProperty("user.home") + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup");
-        File main = new File("C:/WaterReminder");
-        main.mkdirs();
+        File file = new File(System.getProperty("user.home") + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/WaterReminder.jar");
 
         InputStream in = Main.class.getResourceAsStream("/WaterReminder.jar");
-        OutputStream out = new FileOutputStream(new File(main, "WaterReminder.jar"));
+        OutputStream out = new FileOutputStream(file);
         byte[] buffer = new byte[1024];
         int bytesRead = 0;
         while ((bytesRead = in.read(buffer)) > 0) {
@@ -25,12 +23,8 @@ public class Main {
         }
         out.close();
 
-        out = new FileOutputStream(new File(startupFolder, "startwater.bat"));
-        out.write("java -jar \"C:/WaterReminder/WaterReminder.jar\"".getBytes());
-        out.close();
-
         if (JOptionPane.showConfirmDialog(null, "The installation is successful!\nWould you like to run the app?", "Water Reminder Installer", JOptionPane.YES_NO_OPTION) == 0) {
-            Runtime.getRuntime().exec("java -jar \"C:/WaterReminder/WaterReminder.jar\"");
+            Runtime.getRuntime().exec("java -jar \"" + file.getAbsolutePath() + "\"");
         }
     }
 }
